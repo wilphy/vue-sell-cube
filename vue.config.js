2813edc26ab737316c905ca6335d61907de7a001
wilphy <149201216@qq.com>
2020-06-07 21:34:34 +0800

    init：mock data

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +18,28 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', (req, res) => {
+        res.json({
+          error: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', (req, res) => {
+        res.json({
+          error: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', (req, res) => {
+        res.json({
+          error: 0,
+          data: ratings
+        })
+      })
     }
   }
 }
