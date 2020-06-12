29af5b3d5784a7f89b8b101b1b519acd4f9eaf38
wilphy <149201216@qq.com>
2020-06-12 23:13:31 +0800

    feat：tab

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
     <w-header :seller="seller"></w-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
-import WHeader from "./components/w-header/w-header.vue"
+import WHeader from "./components/w-header/w-header"
+import Goods from "./components/goods/goods"
+import Ratings from "./components/ratings/ratings"
+import Seller from "./components/seller/seller"
+import Tab from "./components/tab/tab"
+
 import { getSeller } from "./api/index"
 
 export default {
   name: "App",
   components: {
     WHeader,
+    Tab,
   },
   data() {
     return {
@@ -28,7 +37,24 @@ export default {
       })
     },
   },
+  computed: {
+    tabs() {
+      return [
+        { label: "商品", components: Goods, data: { seller: this.seller } },
+        { label: "评价", components: Ratings, data: { seller: this.seller } },
+        { label: "商家", components: Seller, data: { seller: this.seller } },
+      ]
+    },
+  },
 }
 </script>
 
-<style></style>
+<style>
+#app .tab-wrapper {
+  position: fixed;
+  top: 136px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
